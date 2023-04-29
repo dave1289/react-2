0c00a5c2449dcd02e7e4f23d3e13a40e662dff34
dave-home-2 <davemcelhaney@gmail.com>
2023-04-29 11:27:10 -0400

    got snackorbooze running with updates and audit fixes

--- unroll/unroll.test.js
+++ unroll/unroll.test.js
@@ -7,12 +7,6 @@ const square = [
    [13, 14, 15, 16]
  ];
 
- const smallerSquare = [
-   ["a", "b", "c"],
-   ["d", "e", "f"],
-   ["g", "h", "i"]
- ];
-
 describe("#unroll", function () {
 
   it("is a function", function () {
@@ -21,9 +15,5 @@ describe("#unroll", function () {
 
   it("unrolls array square", function() {
    expect(unroll(square)).toEqual([1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10])
-  })
-
-  it("unrolls smaller array square", function() {
-   expect(unroll(smallerSquare)).toEqual(["a", "b", "c", "f", "i", "h", "g", "d", "e"])
   })
 });
